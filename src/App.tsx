c1570a96dbf958bd2122b026bc587a737fc5b6e6
chaniNimasha <chaninimasha123@gmail.com>
2023-05-09 22:45:50 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React, { useState } from 'react';
-
 import { createStackNavigator } from '@react-navigation/stack';
-
-
 import DrawerNavigator from './DrawerNavigator';
 import Login from './Login';
 import { NavigationContainer } from '@react-navigation/native';
-
-
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Drawer" component={DrawerNavigator} />
-      </Stack.Navigator>
-      </NavigationContainer>
-    
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="Drawer" component={DrawerNavigator} options={{headerShown: false}}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,34 +1,37 @@
 import React, { useState, Component } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
-import DrawerNavigator from './DrawerNavigator';
 import { useNavigation } from '@react-navigation/native';
 
-export const Login = () => {
+export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [keepSignedIn, setKeepSignedIn] = useState(false);
-  
-  const navigation = useNavigation();
-  
-  const goToDetails = () => {
-    navigation.navigate('Drawer');
-  };
 
+  const onPressHandler = () => {
+    // navigation.navigate('Screen_A');
+    navigation.goBack();
+  }
+  // const navigation = useNavigation();
+  const navigation = useNavigation();
+  const login = () => {
+    // navigation.navigate('Screen_A');
+     navigation.navigate('Drawer');
+  }
   const handleForgotPassword = () => {
-    
+
   };
 
   return (
     <View style={styles.container}>
-      
-    <Image source={require('../assets/tea.png')} style={styles.backgroundImage} />
-    <Image
-      source={ require('../assets/logo.png') }
-      style={{ width: 260, height: 90,top:-15 }}
-    />
-     
-     <Text style={styles.Title}>Fazenda Dashboard</Text>
+
+      <Image source={require('../assets/tea.png')} style={styles.backgroundImage} />
+      <Image
+        source={require('../assets/logo.png')}
+        style={{ width: 260, height: 90, top: -15 }}
+      />
+
+      <Text style={styles.Title}>Fazenda Dashboard</Text>
       <Text style={styles.heading}>SIGN IN</Text>
       <Text style={styles.username}>USERNAME</Text>
       <TextInput
@@ -38,14 +41,14 @@ export const Login = () => {
         placeholder="Enter name"
         placeholderTextColor="black"
       />
-       <Text style={styles.username}>PASSWOARD</Text>
+      <Text style={styles.username}>PASSWOARD</Text>
       <TextInput
         style={styles.input}
         onChangeText={text => setPassword(text)}
         value={password}
         placeholder="Enter Password"
         placeholderTextColor="black"
-      
+
       />
       <TouchableOpacity onPress={handleForgotPassword}>
         <Text style={styles.forgotPassword}>Forgot Password?</Text>
@@ -58,12 +61,12 @@ export const Login = () => {
         />
         <Text style={styles.keepSignedIn}>Keep me signed in</Text>
       </View>
-      <TouchableOpacity style={styles.loginButton} 
-      onPress={goToDetails}
+      <TouchableOpacity style={styles.loginButton}
+        onPress={login}
       >
         <Text style={styles.loginButtonText}>SIGN IN</Text>
       </TouchableOpacity>
-     
+
     </View>
   );
 };
@@ -74,44 +77,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  username:{
+  username: {
     fontSize: 18,
-   color:"rgba(255, 255, 255, 0.5)",
-   right:117,
-   bottom:-24,
+    color: "rgba(255, 255, 255, 0.5)",
+    right: 117,
+    bottom: -24,
     marginBottom: 32,
     fontWeight: 'bold',
 
 
 
   },
-  Title:{ fontSize: 24,
-   color:"white",
-   textAlign:'center',
-   top:-10,
+  Title: {
+    fontSize: 24,
+    color: "white",
+    textAlign: 'center',
+    top: -10,
     marginBottom: 32,
 
   },
   backgroundImage: {
     flex: 5,
-    resizeMode:'cover',
-    top:-80,
-    right:-180,
-   // bottom:100,
+    resizeMode: 'cover',
+    top: -80,
+    right: -180,
+    // bottom:100,
     position: 'absolute',
     width: "200%",
     flexDirection: 'column'
   },
 
-  
+
   heading: {
     fontSize: 29,
     fontWeight: 'bold',
-    color:"white",
-    right:120,
-    textDecorationLine:"underline",
-    textDecorationColor:"blue",
-    bottom:-25,
+    color: "white",
+    right: 120,
+    textDecorationLine: "underline",
+    textDecorationColor: "blue",
+    bottom: -25,
     marginBottom: 32,
   },
   input: {
@@ -123,14 +127,14 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingHorizontal: 16,
     marginBottom: 16,
-    
-    
+
+
   },
   forgotPassword: {
-    color:"rgba(255, 255, 255, 0.5)",
+    color: "rgba(255, 255, 255, 0.5)",
     marginBottom: 16,
-    fontSize:18,
-    right:90,
+    fontSize: 18,
+    right: 90,
   },
   checkboxContainer: {
     flexDirection: 'row',
@@ -142,9 +146,9 @@ const styles = StyleSheet.create({
   },
   keepSignedIn: {
     fontSize: 17,
-    right:167,
-    fontWeight:"bold",
-    color:"rgba(255, 255, 255, 0.5)"
+    right: 167,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 0.5)"
   },
   loginButton: {
     backgroundColor: '#1161ee',
@@ -158,4 +162,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Login;
+
